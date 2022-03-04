1d0890c752d339a235223caabecf026436124527
Grant Kinney <hi@grant.mk>
2022-03-04 09:57:44 -0600

    Swap screens for LEDs

--- kyria/qmk/rules.mk
+++ kyria/qmk/rules.mk
@@ -2,5 +2,5 @@ COMBO_ENABLE = yes         # https://docs.qmk.fm/#/feature_combo
 VPATH += keyboards/gboards # http://combos.gboards.ca/
 ENCODER_ENABLE = yes       # https://docs.qmk.fm/#/feature_encoders
 MOUSEKEY_ENABLE = no       # https://docs.qmk.fm/#/feature_mouse_keys
-OLED_ENABLE = yes          # https://docs.qmk.fm/#/feature_oled_driver
-RGBLIGHT_ENABLE = no       # https://docs.qmk.fm/#/feature_rgblight
+OLED_ENABLE = no           # https://docs.qmk.fm/#/feature_oled_driver
+RGBLIGHT_ENABLE = yes      # https://docs.qmk.fm/#/feature_rgblight
