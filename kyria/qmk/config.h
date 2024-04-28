bd747b8f5c0c439cc31981238cefde9af2ae7872
Grant Kinney <hi@grant.mk>
2024-04-28 16:52:41 -0500

    Disables quick tap on mod-tap keys

--- kyria/qmk/config.h
+++ kyria/qmk/config.h
@@ -23,7 +23,7 @@
 #define TAPPING_TERM 200
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
+#define QUICK_TAP_TERM 0
 
 // Apply the modifier on keys that are tapped during a short hold of a modtap
 #define PERMISSIVE_HOLD
