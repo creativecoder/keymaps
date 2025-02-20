babe8ce11a7844de055fae4264d39b94c998d2c8
Grant Kinney <hi@grant.mk>
2025-02-20 15:40:10 -0600

    Updates numpad layout to match standard order left-to-right

--- kyria/qmk/keymap.c
+++ kyria/qmk/keymap.c
@@ -116,9 +116,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         U_NU,    U_NA,    U_NA,    U_NA,    U_NA,    U_NU,    KC_MSTP, KC_MPLY,  KC_MUTE, KC_MUTE
     ),
     [NUM] = LAYOUT_kyria_3x5(
-      KC_CIRC, KC_9,    KC_8,    KC_7,    KC_SLSH,                                     U_NA,    U_NA,    U_NA,    U_NA,    QK_RBT,
-      KC_EXLM, KC_3,    KC_2,    KC_1,    KC_ASTR,                                     U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-      KC_DOT,  KC_6,    KC_5,    KC_4,    KC_PLUS, KC_SPC,  DBLARR,  U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
+      KC_CIRC, KC_7,    KC_8,    KC_9,    KC_SLSH,                                     U_NA,    U_NA,    U_NA,    U_NA,    QK_RBT,
+      KC_EXLM, KC_1,    KC_2,    KC_3,    KC_ASTR,                                     U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+      KC_DOT,  KC_4,    KC_5,    KC_6,    KC_PLUS, KC_SPC,  DBLARR,  U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
                         U_NU,    KC_EQL,  KC_0,    KC_MINS, SNGARR,  U_NA,    U_NA,    U_NA,    U_NA,    U_NU
     ),
     [SYM] = LAYOUT_kyria_3x5(
